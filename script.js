8b529c34b15181fc8f202ec59a3b4cbcfdf62683
gino-lr <gino.liza@ucsp.edu.pe>
2023-06-26 13:45:32 -0500

    Multi tempo metronome

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const audio = new Audio('click.mp3');
 let intervalId;
+let tempo = 120;
 
 function playClick() {
   audio.currentTime = 0;
@@ -7,7 +8,6 @@ function playClick() {
 }
 
 function startMetronome() {
-  const tempo = document.getElementById('tempo').value;
   const delay = 60000 / tempo;
   intervalId = setInterval(playClick, delay);
 }
@@ -16,7 +16,17 @@ function stopMetronome() {
   clearInterval(intervalId);
 }
 
+function setTempo(newTempo) {
+  tempo = newTempo;
+  document.getElementById('tempo-display').textContent = `${tempo} BPM`;
+}
+
 const startButton = document.getElementById('start');
 const stopButton = document.getElementById('stop');
+const tempo90Button = document.getElementById('tempo-90');
+const tempo150Button = document.getElementById('tempo-150');
 startButton.addEventListener('click', startMetronome);
 stopButton.addEventListener('click', stopMetronome);
+tempo90Button.addEventListener('click', () => setTempo(90));
+tempo150Button.addEventListener('click', () => setTempo(150));
+setTempo(tempo);
