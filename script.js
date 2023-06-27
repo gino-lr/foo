39c17227dec969a156aa82e25575f99b278cff4d
gino-lr <gino.liza@ucsp.edu.pe>
2023-06-27 13:41:45 -0500

    Functionality complete. Lacks style

--- script.js
+++ script.js
@@ -1,42 +1,82 @@
 const audio = new Audio('click.mp3');
 let intervalId;
-let tempo = 100;
+
+let tempo1 = 90;
+let tempo2 = 150;
 
 function playClick() {
     audio.currentTime = 0;
     audio.play();
 }
 
-function startMetronome() {
-    const delay = 60000 / tempo;
-    intervalId = setInterval(playClick, delay);    
+function startMetronome(_tempo) {
+    const delay = 60000 / _tempo;
+    intervalId = setInterval(playClick, delay);
 }
 
 function stopMetronome() {
     clearInterval(intervalId);
 }
 
-function setTempo(newTempo) {
-    tempo = newTempo;
-    document.getElementById('tempo-display').textContent = `${tempo} BPM`;
-    console.log(tempo);
-}
 
+// STOP 
 const stopButton = document.getElementById('stop');
+stopButton.addEventListener('click', stopMetronome);
+
+// METRONOMO 1 Y 2
 const tempoButton1 = document.getElementById('tempo-1');
 const tempoButton2 = document.getElementById('tempo-2');
-stopButton.addEventListener('click', stopMetronome);
+
 tempoButton1.addEventListener('click', () => {
-    tempo = 90;
     stopMetronome();
-    startMetronome();
-    setTempo(tempo);
+    startMetronome(tempo1);
+    console.log(tempo1);
 });
 tempoButton2.addEventListener('click', () => {
-    tempo = 150;
     stopMetronome();
-    startMetronome();
-    setTempo(tempo);
+    startMetronome(tempo2);
+    console.log(tempo2);
 });
 
-setTempo(tempo);
+// Inrease and decrease tempo
+const decreaseButton1 = document.getElementById("tempo-1-decrease");
+const increaseButton1 = document.getElementById("tempo-1-increase");
+
+const decreaseButton2 = document.getElementById("tempo-2-decrease");
+const increaseButton2 = document.getElementById("tempo-2-increase");
+
+decreaseButton1.addEventListener("click", () => {
+    tempo1 -= 10;
+    console.log(tempo1);
+    tempoButton1.textContent = `${tempo1} BPM`;
+    stopMetronome();
+    startMetronome(tempo1);
+
+});
+
+increaseButton1.addEventListener("click", () => {
+    tempo1 += 10;
+    console.log(tempo1);
+    tempoButton1.textContent = `${tempo1} BPM`;
+    stopMetronome();
+    startMetronome(tempo1);
+
+});
+
+decreaseButton2.addEventListener("click", () => {
+    tempo2 -= 10;
+    console.log(tempo2);
+    tempoButton2.textContent = `${tempo2} BPM`;
+    stopMetronome();
+    startMetronome(tempo2);
+
+});
+
+increaseButton2.addEventListener("click", () => {
+    tempo2 += 10;
+    console.log(tempo2);
+    tempoButton2.textContent = `${tempo2} BPM`;
+    stopMetronome();
+    startMetronome(tempo2);
+
+});
